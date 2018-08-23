ebbbe1b993d4d7f0a99a8bca75f16a5da6c8762c
Vesta <Irina@Irina-PC>
2018-08-23 22:19:37 +0300

    Вопросы

--- test_upload.py
+++ test_upload.py
@@ -16,15 +16,20 @@ fls = os.listdir('./')
 fname = "t.pdf"
 print()
 print("<br />")
-token = ""
+f = open('conf.json')
+j = json.loads(f.read())
+token = j["token"]
 uid = 4756
 heads = {'Authorization': 'Bearer ' + token, 'User-Agent': 'Python3. VestaXlsxApp (test@huntflow.ru)', "X-File-Parse": "true"}
 # heads['Content-Type'] = "multipart/form-data"
 # post = {"file": open("Тестовое задание/Frontend-разработчик/Танский Михаил.pdf", 'rb')} #.encode('utf-8')
 post = {"file": open(fname, 'rb')} #
 # r = requests.post('/python/testhf/test_post.php', headers=heads, files=post) # https://api.huntflow.ru/account/4756/upload
-r = requests.post('https://api.huntflow.ru/account/" + str(uid) + "/upload', headers=heads, files=post) # https://api.huntflow.ru/account/4756/upload
-print(r.text + "<br />")
+r = requests.post('https://api.huntflow.ru/account/' + str(uid) + '/upload', headers=heads, files=post) # https://api.huntflow.ru/account/4756/upload
+print(r.text + "<br />") # {"errors": [{"type": "upload", "value": "unable_to_upload"}]}
 # print(json.loads(r.text))
 # print(r.request.headers)
 print("<br />")
+
+# Вопрос 1: что не так к моменту выполнения строки 28 или в ней самой?
+# Вопрос 2: Что читать по работе с файлами с не ascii путями как в строке 25?
