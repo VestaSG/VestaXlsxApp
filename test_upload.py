dd40d4d46d4babeb1b8253cf4977b9f0a3c2aa08
Vesta <Irina@Irina-PC>
2018-08-24 23:40:43 +0300

    Только один вопрос

--- test_upload.py
+++ test_upload.py
@@ -10,10 +10,10 @@ import requests
 import os
 import json
 
-print("<br />")
 fls = os.listdir('./')
 # fname = fls[7]
 fname = "t.pdf"
+fname = "Тестовое задание/Frontend-разработчик/Танский Михаил.pdf".encode('utf-8')
 print()
 print("<br />")
 f = open('conf.json')
@@ -22,8 +22,14 @@ token = j["token"]
 uid = 4756
 heads = {'Authorization': 'Bearer ' + token, 'User-Agent': 'Python3. VestaXlsxApp (test@huntflow.ru)', "X-File-Parse": "true"}
 # heads['Content-Type'] = "multipart/form-data"
-# post = {"file": open("Тестовое задание/Frontend-разработчик/Танский Михаил.pdf", 'rb')} #.encode('utf-8')
-post = {"file": open(fname, 'rb')} #
+f1 = open(fname, 'rb')
+fcontent = f1.read()
+print()
+print("<br />")
+
+# post = {"file": f1} #
+# TODO: брать расширение из имени файла
+post = {'file': ('rt.pdf', fcontent)}
 # r = requests.post('/python/testhf/test_post.php', headers=heads, files=post) # https://api.huntflow.ru/account/4756/upload
 r = requests.post('https://api.huntflow.ru/account/' + str(uid) + '/upload', headers=heads, files=post) # https://api.huntflow.ru/account/4756/upload
 print(r.text + "<br />") # {"errors": [{"type": "upload", "value": "unable_to_upload"}]}
@@ -31,5 +37,6 @@ print(r.text + "<br />") # {"errors": [{"type": "upload", "value": "unable_to_up
 # print(r.request.headers)
 print("<br />")
 
-# Вопрос 1: что не так к моменту выполнения строки 28 или в ней самой?
-# Вопрос 2: Что читать по работе с файлами с не ascii путями как в строке 25?
+print(sys.getfilesystemencoding())
+
+# Вопрос 1: что не так к моменту выполнения строки 34 или в ней самой? Почему ответ {"errors": [{"type": "upload", "value": "unable_to_upload"}]} ?
